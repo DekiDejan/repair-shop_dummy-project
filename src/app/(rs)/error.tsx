555dbf69bdda26af2ad3c151f8fecea543ed05bb
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-11-03 11:29:51 +0100

    setup error pages

---
+++ src/app/(rs)/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
+
+export default function ErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to Sentry
+    console.error(error);
+
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
